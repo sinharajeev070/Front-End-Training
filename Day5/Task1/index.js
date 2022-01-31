8a6719858fd277dece241e21802a228756ec8d15
Rajeev <sinharajeev070@gmail.com>
2022-01-31 12:27:19 +0530

    new file:   Day5/Task1/index.html 	new file:   Day5/Task1/index.js

---
+++ Day5/Task1/index.js
@@ -0,0 +1,7 @@
+console.log("Calculator")
+var a = 28, b = 20
+console.log("Sum: " + (a+b))
+console.log("Difference: " + (a-b))
+console.log("Multiplication: " + (a*b))
+console.log("Division: " + (a/b))
+console.log("Modulus: " + (a%b))
